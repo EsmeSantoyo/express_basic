027455463853bb8df6f147656bb98be2c8df4b91
Esmeralda Santoyo <58286718+EsmeSantoyo@users.noreply.github.com>
2022-05-01 16:07:05 -0500

    Add: Agregar una nueva ruta

--- express_basic/app.js
+++ express_basic/app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send('hello world')
 })
 
+app.get("/launchx", (req, res) => {
+    res.send('Bienvenido a launchx')
+})
+
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`)
 })
